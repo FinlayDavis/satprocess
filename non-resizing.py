44141cbee41cccf0f9ebcb2cbb46cdcae5399091
FinlayDavis <finlayjdavis@gmail.com>
2024-12-31 14:30:23 +0000

    Code now outputs a slideshow displaying the newly aligned images, for ease of comparison.

--- non-resizing.py
+++ non-resizing.py
@@ -1,5 +1,6 @@
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import scipy.ndimage as sp
 from skimage.transform import hough_circle, hough_circle_peaks
@@ -95,18 +96,23 @@ def process_and_align_images(folder_path: str, shifts_file: str, save_folder: st
     # Read the first image as the reference
     ref_file = files[default_file]
     ref_path = os.path.join(folder_path, ref_file)
+    ref_array = circleIdentify(ref_path)
 
     if ref_file in shifts:
         ref_shift_x, ref_shift_y = shifts[ref_file]
         ref_centroid = (ref_shift_x, ref_shift_y)
     else:   
-        ref_array = circleIdentify(ref_path)
         ref_edges = preprocess(ref_array)
         ref_circles = houghTransform(ref_edges, ref_array)
         ref_centroid = ref_circles[0][:2]
         shifts[ref_file] = (ref_centroid[1], ref_centroid[0])
     
     aligned_images = []
+
+    save_path = os.path.join(save_folder, ref_file)
+
+    save_aligned_images(ref_file, ref_array, save_path)
+    aligned_images.append((ref_file, ref_array))
     
     # Process and align all images
     for file in files[1:]: # Skip the first file as it's the reference file
@@ -125,22 +131,9 @@ def process_and_align_images(folder_path: str, shifts_file: str, save_folder: st
             shifts[file] = (shift_y, shift_x)
         
         aligned_image = alignImages(file_array, shift_y, shift_x)
-
         save_path = os.path.join(save_folder, file)
-
-        # Save new arrays as .fits data
-        fits.writeto(save_path, aligned_image, overwrite=True)
-
-        # Save a .png of the figure
-        fig, ax = plt.subplots()
-        ax.imshow(aligned_image, cmap='gray')
-        ax.set_title(file)
-        ax.axis('off')
-        fig_save_path = os.path.join(save_folder, f"{os.path.splitext(file)[0]}.png")
-        fig.savefig(fig_save_path)
-        plt.close(fig)
-
-        aligned_images.append((file, aligned_image))
+        save_aligned_images(file, aligned_image, save_path)
+        aligned_images.append((file, file_array))
     
     # Include ref file in aligned resulrs
     aligned_images.insert(0, (ref_file, plt.imread(ref_path)))
@@ -150,16 +143,54 @@ def process_and_align_images(folder_path: str, shifts_file: str, save_folder: st
 
     return aligned_images
 
+def save_aligned_images(file, array, save_path):
+    # Save new arrays as .fits data
+    fits.writeto(save_path, array, overwrite=True)
+
+    # Save a .png of the figure
+    fig, ax = plt.subplots()
+    ax.imshow(array, cmap='gray')
+    ax.set_title(file)
+    ax.axis('off')
+    fig_save_path = os.path.join(save_folder, f"{os.path.splitext(file)[0]}.png")
+    fig.savefig(fig_save_path)
+    plt.close(fig)
+
+def load_aligned_images(folder):
+    files = [f for f in os.listdir(folder) if f.endswith('.fits')]
+    images = []
+    for file in files:
+        file_path = os.path.join(folder, file)
+        image = fits.getdata(file_path)
+        images.append((file, image))
+    return images
+
+def create_slideshow(images, interval=1000):
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    
+    def update(frame):
+        ax.clear()
+        ax.imshow(images[frame][1], cmap='gray')
+        ax.set_title(images[frame][0])
+        ax.axis('off')
+    
+    ani = FuncAnimation(fig, update, frames=len(images), interval=interval, repeat=True)
+    
+    def on_key_press(event):
+        if event.key=='q':
+            plt.close(fig)
+
+    fig.canvas.mpl_connect('key_press_event', on_key_press)
+
+    plt.show()
+
 # Example usage
 folder_path = 'TestImages'
 shifts_file = folder_path + '/shifts.csv'
 save_folder = folder_path + '/alignedImages'
 aligned_images = process_and_align_images(folder_path, shifts_file, save_folder)
 
-# # Visualize the aligned results
-# fig, axes = plt.subplots(1, len(aligned_images), figsize=(15, 6))
-# for ax, (file, aligned_image) in zip(axes, aligned_images):
-#     ax.imshow(aligned_image, cmap='gray')
-#     ax.set_title(file)
-#     ax.axis('off')
-# plt.show()
+# Example usage
+aligned_images = load_aligned_images(save_folder)
+create_slideshow(aligned_images)
